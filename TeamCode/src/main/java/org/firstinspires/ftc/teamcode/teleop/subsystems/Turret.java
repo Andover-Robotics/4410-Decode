62b823f50327e5aeb5a1a42f084014494deb23a5
Raahil Parikh <raahilparikh@gmail.com>
2026-01-26 23:55:58 -0500

    YAYY auto calc rapid fire sleep works!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/subsystems/Turret.java
@@ -31,7 +31,7 @@ public class Turret {
     public static double POS_TRACK_Y = 0;
     public static double TURRET_OFFSET_BACK_IN = 1; // inches back from robot center
     public static double rapidFireDistanceThresholdIn = 87;
-    public static double rapidFireSleepScalePerIn = 0.01 ;
+    public static double rapidFireSleepScalePerIn = 0.009584479 ;
     public static double
             largeP = 0.006, largeI = 0, largeD = 0.0003,
             smallP = 0.0125, smallI = 0, smallD = 0.0004,
