b54b697ff6b88f29dfbb1e94b7f77b15c62dad0a
Raahil Parikh <raahilparikh@gmail.com>
2026-01-27 00:01:34 -0500

    added failsafe intaking to mainteleop :oo overrides sensors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleop.java
@@ -143,14 +143,16 @@ public class MainTeleop extends LinearOpMode {
                     bot.intake();
                     gp1.gamepad.stopRumble();
                 }
-            } else if (gp1.isDown(GamepadKeys.Button.LEFT_BUMPER)){
+            } else if (gp1.isDown(GamepadKeys.Button.LEFT_BUMPER)) {
                 bot.reverseIntake();
-                if (bot.indexer.countBalls()==3) {
+                if (bot.indexer.countBalls() == 3) {
                     gp1.gamepad.rumble(1, 1, -1);
                 } else {
                     bot.reverseIntake();
                     gp1.gamepad.stopRumble();
                 }
+            } else if (gp1.isDown(GamepadKeys.Button.RIGHT_BUMPER)) {
+                bot.intake();
             } else {
                 bot.stopIntake();
                 gp1.gamepad.stopRumble();
